6e2bca1c5e0faf1831cc85e99202bb7a7c4bf66f
pepelu <joseluis.aybar.gutierrez@gmail.com>
2026-01-16 14:22:35 +0100

    Progrmación dia16/01

---
+++ PR_ENº2/practicaEj1/src/practicaej1/Alquiler.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaej1;
+import practicaej1.Vehiculo;
+/**
+ *
+ * @author EAG
+ */
+public class Alquiler {
+// Declaración de atributos
+    Vehiculos[]: Vehiculo;
+    
+// Constructor por defecto
+// Constructor por parámetros
+// Constructor de copia
+// Declaración de getters (1 por atributo)
+// Declaración de setters (1 por atributo)
+// Declaración de métodos personalizados
+// Declaración toString() (con @Override)
+}
---
+++ PR_ENº2/practicaEj1/src/practicaej1/Vehiculo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaej1;
+
+/**
+ *
+ * @author EAG
+ */
+public class Vehiculo {
+// Declaración de atributos
+ 
+    private String matricula, marca, modelo;
+    private boolean cocheDisponible;
+
+// Constructor por defecto
+    public Vehiculo(){
+        this.matricula = "";
+        this.marca = "";
+        this.modelo = "";
+        this.cocheDisponible = true;
+    }
+    
+// Constructor por parámetros
+    public vehiculo(String matriculaParametro, String marcaParametro, String modeloParametro, boolean cocheDisponibleParametro){
+    
+}
+// Constructor de copia
+// Declaración de getters (1 por atributo)
+// Declaración de setters (1 por atributo)
+// Declaración de métodos personalizados
+// Declaración toString() (con @Override) 
+}
