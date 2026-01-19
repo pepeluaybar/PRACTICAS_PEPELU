02400b215b513112b83f650288eaacb064c5b55a
pepelu <joseluis.aybar.gutierrez@gmail.com>
2026-01-19 14:29:27 +0100

    update 2

--- PR_ENº2/practicaEj1/src/practicaej1/Vehiculo.java
+++ PR_ENº2/practicaEj1/src/practicaej1/Vehiculo.java
@@ -24,12 +24,61 @@ public class Vehiculo {
     }
     
 // Constructor por parámetros
-    public vehiculo(String matriculaParametro, String marcaParametro, String modeloParametro, boolean cocheDisponibleParametro){
-    
-}
+    public Vehiculo(String matricula, String marca, String modelo, boolean cocheDisponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cocheDisponible = cocheDisponible;
+    }
 // Constructor de copia
+    public Vehiculo(Vehiculo v){
+        this.matricula = v.matricula;
+        this.marca = v.marca;
+        this.modelo = v.modelo;
+        this.cocheDisponible = v.cocheDisponible;
+    }
 // Declaración de getters (1 por atributo)
+    public String getMatricula(){
+        return this.matricula;
+    }
+    public String getMarca(){
+        return this.marca;
+    }
+    public String getModelo(){
+        return this.modelo;
+    }    
+    public boolean getCocheDisponible(){
+        return this.cocheDisponible;
+    }    
 // Declaración de setters (1 por atributo)
+    public void setMatricula(String matricula){
+         this.matricula = matricula;
+    }
+    public void setMarca(String marca){
+         this.marca = marca;
+    }
+    public void setModelo(String modelo){
+         this.modelo = modelo;
+    }    
+    public void setCocheDisponible(boolean cocheDisponible){
+         this.cocheDisponible = cocheDisponible;
+    } 
 // Declaración de métodos personalizados
+    
 // Declaración toString() (con @Override) 
+    @Override
+        public String toString() {
+            String cadena = "Matricula: " + matricula +
+                            ", Marca: " + marca +
+                            ", Modelo: " + modelo +
+                            ", Disponible: ";
+
+            if (cocheDisponible) {
+                cadena += "sí";
+            } else {
+                cadena += "no";
+            }
+
+            return cadena;
+}
 }
