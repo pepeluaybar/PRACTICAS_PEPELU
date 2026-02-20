109f77dceb45f1824ae416def2a1c07178a395c0
pepelu <joseluis.aybar.gutierrez@gmail.com>
2026-02-20 13:34:27 +0100

    Refactor tipos de deporte y medalla a enums

--- PR_ENº2/PracticaPrEn2/src/practicapren2/Medalla.java
+++ PR_ENº2/PracticaPrEn2/src/practicapren2/Medalla.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicapren2;
 
-/**
- *
- * @author EAG
- */
+import java.util.Objects;
+
 public class Medalla {
-    
+
+    private final TipoMedalla tipo;
+    private final Prueba prueba;
+
+    public Medalla(TipoMedalla tipo, Prueba prueba) {
+        this.tipo = Objects.requireNonNull(tipo, "El tipo de medalla es obligatorio");
+        this.prueba = Objects.requireNonNull(prueba, "La prueba asociada es obligatoria");
+    }
+
+    public TipoMedalla getTipo() {
+        return tipo;
+    }
+
+    public Prueba getPrueba() {
+        return prueba;
+    }
+
+    public boolean esOro() {
+        return tipo == TipoMedalla.ORO;
+    }
 }
