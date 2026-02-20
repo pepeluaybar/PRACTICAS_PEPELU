34617d79fd2a220e0ef8fc735531ae73a68761ed
pepelu <joseluis.aybar.gutierrez@gmail.com>
2026-02-20 13:33:49 +0100

    Refactor participantes con clase base abstracta

--- PR_ENº2/PracticaPrEn2/src/practicapren2/Deportista.java
+++ PR_ENº2/PracticaPrEn2/src/practicapren2/Deportista.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicapren2;
 
-/**
- *
- * @author EAG
- */
-public class Deportista {
-    
+public class Deportista extends Participante {
+
+    public Deportista(String nombre, String pais, String idOlimpico) {
+        super(nombre, pais, idOlimpico);
+    }
 }
--- PR_ENº2/PracticaPrEn2/src/practicapren2/Participante.java
+++ PR_ENº2/PracticaPrEn2/src/practicapren2/Participante.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicapren2;
 
-/**
- *
- * @author EAG
- */
-public class abstract Participante {
-    
+public abstract class Participante {
+
+    private String nombre;
+    private String pais;
+    private String idOlimpico;
+    private int totalMedallas;
+
+    protected Participante(String nombre, String pais, String idOlimpico) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.idOlimpico = idOlimpico;
+        this.totalMedallas = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getIdOlimpico() {
+        return idOlimpico;
+    }
+
+    public void setIdOlimpico(String idOlimpico) {
+        this.idOlimpico = idOlimpico;
+    }
+
+    public int getTotalMedallas() {
+        return totalMedallas;
+    }
+
+    public void setTotalMedallas(int totalMedallas) {
+        this.totalMedallas = totalMedallas;
+    }
+
+    public void incrementarMedallas() {
+        totalMedallas++;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "nombre='" + nombre + '\'' +
+                ", pais='" + pais + '\'' +
+                ", idOlimpico='" + idOlimpico + '\'' +
+                ", totalMedallas=" + totalMedallas +
+                '}';
+    }
 }
